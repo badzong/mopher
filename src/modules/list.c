a5f3f2d4521753bec5ec7a9eef3d7a5df76c6a91
Manuel Badzong <manuel@badzong.com>
2009-12-15 12:51:27 +0100

    Added list.c
      * list.c contains acl list functions

---
+++ src/modules/list.c
@@ -0,0 +1,57 @@
+#include "mopher.h"
+
+
+static var_t *
+list_contains(ll_t *args)
+{
+	var_t *haystack, *needle, *v;
+	ll_t *list;
+	VAR_INT_T match = 0;
+
+	haystack = ll_next(args);
+	needle = ll_next(args);
+
+	if (haystack == NULL || needle == NULL)
+	{
+		log_error(
+		    "list_contains: usage: list_contains(haystack, needle)");
+		return NULL;
+	}
+
+	if (haystack->v_type != VT_LIST)
+	{
+		log_error("list_contains: first argument needs to be a list");
+		return NULL;
+	}
+
+	list = haystack->v_data;
+	ll_rewind(list);
+
+	while ((v = ll_next(list)))
+	{
+		if (var_compare(needle, v) == 0)
+		{
+			match = 1;
+			break;
+		}
+	}
+
+	v = var_create(VT_INT, NULL, &match, VF_COPYDATA);
+	if (v == NULL)
+	{
+		log_error("list_contains: var_create failed");
+		return NULL;
+	}
+
+	return v;
+}
+
+
+int
+list_init(void)
+{
+	acl_function_register("list_contains",
+	    (acl_function_callback_t) list_contains);
+
+	return 0;
+}
