c36145741a92ea0a44c4ccb00a675bed2ff4c9b7
Manuel Badzong <manuel@badzong.com>
2012-05-05 16:06:57 +0200

    Fixed bad _die macros in log.h

--- src/include/log.h
+++ src/include/log.h
@@ -21,13 +21,13 @@ void log_message(int type, var_t *mailspec, char *f, ...);
 #define log_notice(...) log_log(LOG_NOTICE, 0, __VA_ARGS__)
 #define log_error(...) log_log(LOG_ERR, 0, __VA_ARGS__)
 #define log_warning(...) log_log(LOG_WARNING, 0, __VA_ARGS__)
-#define log_die(...) log_exit(LOG_WARNING, 0, __VA_ARGS__)
+#define log_die(RETURN, ...) log_exit(RETURN, 0, __VA_ARGS__)
 
 #define log_sys_debug(...) log_log(LOG_DEBUG, 1, __VA_ARGS__)
 #define log_sys_info(...) log_log(LOG_INFO, 1, __VA_ARGS__)
 #define log_sys_notice(...) log_log(LOG_NOTICE, 1, __VA_ARGS__)
 #define log_sys_error(...) log_log(LOG_ERR, 1, __VA_ARGS__)
 #define log_sys_warning(...) log_log(LOG_WARNING, 1, __VA_ARGS__)
-#define log_sys_die(...) log_exit(LOG_WARNING, 1, __VA_ARGS__)
+#define log_sys_die(RETURN, ...) log_exit(RETURN, 1, __VA_ARGS__)
 
 #endif /* _LOG_H_ */
