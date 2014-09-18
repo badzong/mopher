7a582046a86c8b2b3d529649b4497c2ccca1908e
Manuel Badzong <manuel@andev.ch>
2014-09-18 18:09:29 +0200

    Added more hash functions for testing

--- src/hash.c
+++ src/hash.c
@@ -22,3 +22,35 @@ hash_one_at_a_time(char *key, u_int32_t len)
 
 	return hash;
 }
+
+hash_t
+hash_djb(void *key, u_int32_t len)
+{
+	unsigned char *p = key;
+	unsigned long h = 0;
+	unsigned long i;
+
+	for (i = 0; i < len; i++)
+		h = 33 * h + p[i];
+
+	return h;
+}
+
+hash_t
+hash_test(void *key, u_int32_t len)
+{
+	unsigned char *p = key;
+	unsigned long h = 0;
+	unsigned long i;
+
+	for (i = 0; i < len; i++)
+		h += p[i];
+
+	return h;
+}
+
+hash_t
+hash_chain(void *key, u_int32_t len)
+{
+	return hash_test(key, len) % 5;
+}
--- src/include/hash.h
+++ src/include/hash.h
@@ -6,7 +6,13 @@
 typedef unsigned long hash_t;
 
 hash_t hash_one_at_a_time(char *key, u_int32_t len);
+hash_t hash_djb(void *key, u_int32_t len);
+hash_t hash_test(void *key, u_int32_t len);
+hash_t hash_chain(void *key, u_int32_t len);
 
 #define HASH hash_one_at_a_time
+//#define HASH hash_djb
+//#define HASH hash_test
+//#define HASH hash_chain
 
 #endif
