3e9bd9d343090b6ab6d3598997a48787bf17e50b
Manuel Badzong <manuel@andev.ch>
2014-10-24 10:32:01 +0200

    Changed mopherctl usage for the last time

--- src/mopherctl.c
+++ src/mopherctl.c
@@ -11,42 +11,57 @@
 #undef BINNAME
 #define BINNAME "mopherctl"
 
-#define ACTION_BUCKETS 16
-#define BUFLEN 2048
+#define BUFLEN 4096
 
 static int   moctl_debug = LOG_ERR;
 static char *moctl_config;
 static char *moctl_server;
 static int   moctl_socket;
 
+typedef struct moctl_command {
+	char	*mc_cmdline;
+	char	*mc_srvcmd;
+	int	 mc_args;
+	int	 mc_data;
+} moctl_command_t;
+
+static moctl_command_t moctl_commands[] = {
+	{"dump", "table_dump", 2, 1},
+	{"greylist dump", "greylist_dump", 2, 1},
+	{"greylist pass", "greylist_pass", 5, 0},
+	{NULL, NULL, 0}
+};
+
 static void
 moctl_usage(void)
 {
-	log_error("Usage: %s [-hG] [-d level] [-c file] [-s host] [-D table] [-P <origin> <from> <rcpt>]", BINNAME);
+	log_error("Usage: mopherctl [-h] [-d level] [-c file] [-s host] command");
 	log_error("");
 	log_error("mopherctl controls the mopher daemon.");
 	log_error("");
 	log_error("Available options are:");
 	log_error("");
-	log_error("  -h      Show usage message.");
+	log_error("-h      Show usage message.");
 	log_error("");
-	log_error("  -d level");
-	log_error("    Set logging severity level to level");
+	log_error("-d level");
+	log_error("        Set debug severity level to level");
 	log_error("");
-	log_error("  -c file");
-	log_error("    Read configuration from file.");
+	log_error("-c file");
+	log_error("        Read configuration from file.");
 	log_error("");
-	log_error("  -s host");
-	log_error("    Connect to host.");
+	log_error("-s host");
+	log_error("        Connect to host.");
 	log_error("");
-	log_error("  -D <table>");
-	log_error("    Print raw content of table.");
+	log_error("Available commands are:");
 	log_error("");
-	log_error("  -G");
-	log_error("    Print formatted content of the greylist-table.");
+	log_error("dump <table>");
+	log_error("        Print raw content of table.");
 	log_error("");
-	log_error("  -P <origin> <from> <rcpt>");
-	log_error("    Temporarily whitelist triplet.");
+	log_error("greylist dump");
+	log_error("        Print formatted content of the greylist-table.");
+	log_error("");
+	log_error("greylist pass <origin> <from> <rcpt>");
+	log_error("        Temporarily whitelist triplet.");
 	log_error("");
 
 	exit(EX_USAGE);
@@ -92,79 +107,22 @@ moctl_init(void)
 	return;
 }
 
-static int
-moctl_greylist(void)
-{
-	char *dump;
-	
-	if (server_data_cmd(moctl_socket, "greylist_dump", &dump))
-	{
-		log_die(EX_SOFTWARE, "moctl_greylist_dump: server_data_cmd failed");
-	}
-
-	printf(dump);
-	free(dump);
-
-	return 0;
-}
-
-static int
-moctl_pass(char *source, char *from, char *rcpt)
-{
-	char cmd[BUFLEN];
-	int n;
-	
-	n = snprintf(cmd, sizeof cmd, "greylist_pass %s %s %s", source, from,
-	    rcpt);
-	if (n >= sizeof cmd)
-	{
-		log_die(EX_SOFTWARE, "moctl_greylist_dump: buffer exhausted");
-	}
-
-	if(server_cmd(moctl_socket, cmd))
-	{
-		log_die(EX_SOFTWARE, "moctl_greylist_dump: server_cmd failed");
-	}
-
-	return 0;
-}
-
-static int
-moctl_dump(char *tablename)
-{
-	char *dump;
-	char cmd[BUFLEN];
-	int n;
-
-	n = snprintf(cmd, sizeof cmd, "table_dump %s", tablename);
-	if (n >= sizeof cmd)
-	{
-		log_die(EX_SOFTWARE, "moctl_table_dump: buffer exhausted");
-	}
-	
-	if (server_data_cmd(moctl_socket, cmd, &dump))
-	{
-		log_die(EX_SOFTWARE, "moctl_table_dump: server_data_cmd failed");
-	}
-
-	printf(dump);
-	free(dump);
-
-	return 0;
-}
-
 int
 main(int argc, char **argv)
 {
 	int opt;
-	int dump = 0;
-	int greylist = 0;
-	int pass = 0;
-	int commands = 0;
-	char *table;
-	char *source;
-	char *from;
-	char *rcpt;
+	int i;
+	int len;
+	char cmdline[BUFLEN];
+	char srvcmd[BUFLEN];
+	moctl_command_t *cmd;
+	int index;
+	int args;
+	int r;
+	char *dump = NULL;
+
+	memset(cmdline, 0, sizeof cmdline);
+	memset(srvcmd, 0, sizeof srvcmd);
 
 	// Open log
 	log_init(BINNAME, moctl_debug, 0, 1);
@@ -175,21 +133,11 @@ main(int argc, char **argv)
 		moctl_usage();
 	}
 
-	while ((opt = getopt(argc, argv, "?hGPd:c:s:D:")) != -1) {
+	while ((opt = getopt(argc, argv, "?hd:c:s:")) != -1) {
 		switch(opt) {
 		case '?':
 		case 'h':
 			moctl_usage();
-			break;
-
-		case 'G':
-			++commands;
-			greylist = 1;
-			break;
-
-		case 'P':
-			++commands;
-			pass = 1;
 			break;
 
 		case 'd':
@@ -204,52 +152,72 @@ main(int argc, char **argv)
 			moctl_server = optarg;
 			break;
 
-		case 'D':
-			++commands;
-			dump = 1;
-			table = optarg;
-			break;
-
 		default:
 			moctl_usage();
 			break;
 		}
 	}
 
-	// mopherctl takes exactly 1 command -G | -D | -P
-	if (commands != 1)
+	// Create command string
+	for (i = optind; i < argc; ++i)
+	{
+		len = strlen(cmdline);
+		if (len)
+		{
+			strncat(cmdline, " ", sizeof cmdline - len - 1);
+		}
+
+		len = strlen(cmdline);
+		strncat(cmdline, argv[i], sizeof cmdline - len - 1);
+	}
+	args = i - 1;
+
+	// Translate argv into server command
+	for (cmd = moctl_commands; cmd->mc_cmdline != NULL; ++cmd)
+	{
+		index = strlen(cmd->mc_cmdline);
+		if(strncmp(cmdline, cmd->mc_cmdline, index))
+		{
+			continue;
+		}
+
+		if (args != cmd->mc_args)
+		{
+			moctl_usage();
+		}
+
+		snprintf(srvcmd, sizeof srvcmd, "%s%s", cmd->mc_srvcmd,
+			cmdline + index);
+		break;
+	}
+
+	if (strlen(srvcmd) == 0)
 	{
 		moctl_usage();
 	}
 
 	moctl_init();
 
+	if (cmd->mc_data)
+	{
+		r = server_data_cmd(moctl_socket, srvcmd, &dump);
+	}
+	else
+	{
+		r = server_cmd(moctl_socket, srvcmd);
+	}
+
+	if (r)
+	{
+		log_die(EX_SOFTWARE, "moctl_table_dump: server command failed");
+	}
+
 	if (dump)
 	{
-		return moctl_dump(table);
-	}
-	else if (greylist)
-	{
-		return moctl_greylist();
-	}
-	else if (pass)
-	{
-		if (argc - optind != 3)
-		{
-			moctl_usage();
-			return EX_USAGE;
-		}
-
-		source = argv[optind];
-		from = argv[optind + 1];
-		rcpt = argv[optind + 2];
-
-		return moctl_pass(source, from, rcpt);
+		printf(dump);
+		free(dump);
 	}
 
-	// Impossible
-	moctl_usage();
-	return EX_USAGE;
 
 	// Cleanup
 	if (moctl_socket)
