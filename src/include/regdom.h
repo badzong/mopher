ddd6f0c63ba32860ce1456a0ab998eb9ad449cc7
Manuel Badzong <manuel@andev.ch>
2014-11-09 20:36:35 +0100

    Fixed punycode translation error on NetBSD systems

--- src/include/regdom.h
+++ src/include/regdom.h
@@ -1,20 +1,17 @@
 #ifndef _REGDOM_H_
 #define _REGDOM_H_
 
-#define REGDOM_FREE_NAME 1<<0
-
 struct regdom_rule
 {
 	char *r_name;
 	char  r_exception;
 	char  r_wildcard;
-	int   r_flags;
 };
 typedef struct regdom_rule regdom_rule_t;
 
 void regdom_clear (void);
 void regdom_init (void);
-char * regdom_strdup_idna(char *name);
+int regdom_idna(char *buffer, int size, char *name);
 int regdom_punycode (char *buffer, int size, char* name);
 int regdom_test_init(void);
 void regdom_test (int n);
